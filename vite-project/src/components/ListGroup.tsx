b79965b0fac424e8684e7c86e51cbed88163ee43
Mihai <emailtomihai@gmail.com>
2023-04-12 15:35:30 +0300

    Children Props

--- vite-project/src/components/ListGroup.tsx
+++ vite-project/src/components/ListGroup.tsx
@@ -1,12 +1,17 @@
 import { useState } from "react";
 
-function ListGroup() {
-  let items = ["Bucuresti", "Craiova", "Oradea", "Galati"];
+interface Props {
+  items: string[];
+  heading: string;
+  onSelectItem: (item: string) => void;
+}
+
+function ListGroup({ items, heading, onSelectItem }: Props) {
   const [selectedIndex, setSelectedIndex] = useState(-1);
 
   return (
     <>
-      <h1>List</h1>
+      <h1>{heading}</h1>
       {items.length === 0 && <p>No Items Found!</p>}
       <ul className="list-group">
         {items.map((item, index) => (
@@ -17,7 +22,10 @@ function ListGroup() {
                 ? "list-group-item active"
                 : "list-group-item"
             }
-            onClick={() => setSelectedIndex(index)}
+            onClick={() => {
+              setSelectedIndex(index);
+              onSelectItem(item);
+            }}
           >
             {item}
           </li>
